58f1f4c01355f5a777f6e0ed19d48551305af097
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2016-08-11 15:33:00 +0000

    Missing includes

--- core-1.0/test/asp-net-hello-world/Startup.cs
+++ core-1.0/test/asp-net-hello-world/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace SampleApp
 {
